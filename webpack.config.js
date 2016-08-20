c7ac36b8186b15d96233caff676b4328f92f4fc5
ArturGodunov <artur-godunov@mail.ru>
2016-08-20 11:46:30 +0300

    add bubel

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,16 @@
 
 module.exports = {
     entry: __dirname + "/app/app",
+
     output: {
         path: __dirname + "/app/assets/scripts",
         filename: "bundle.js"
     },
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: "babel?presets[]=es2015"
+        }]
+    }
 };
