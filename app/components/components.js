4c88c985b956ad78174b3dab3dfc8e125acc601e
ArturGodunov <artur-godunov@mail.ru>
2016-10-17 18:07:05 +0300

    remove test ngmodel component

--- app/components/components.js
+++ app/components/components.js
@@ -3,14 +3,12 @@ import angular from 'angular';
 import mainPanel from './main-panel/main-panel';
 import menu from './menu/menu';
 import checkoutPopup from './checkout-popup/checkout-popup';
-import testCustomNgmodel from './test-custom-ngmodel/test-custom-ngmodel';
 
 const components = angular
     .module('app.components', [
         mainPanel.name,
         menu.name,
-        checkoutPopup.name,
-        testCustomNgmodel.name
+        checkoutPopup.name
     ]);
 
 export default components;
--- app/components/test-custom-ngmodel/test-custom-ngmodel.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import testCustomNgmodelComponent from './test-custom-ngmodel.component';
-
-const menu = angular
-    .module('app.component.testCustomNgmodel', [])
-    .component('cTestCustomNgmodel', testCustomNgmodelComponent);
-
-export default menu;
